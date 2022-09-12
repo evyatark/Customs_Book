859797e4d9f1f2ae3bea116d469e2facef993292
evyatar <evyatar@tikalk.com>
2022-09-12 17:31:23 +0300

    add plots for number of agents and number of importers

--- CountPermissions/plotGraphs2.py
+++ CountPermissions/plotGraphs2.py
@@ -41,7 +41,7 @@ def add_count_ishurim(file_df):
     """
     add values in the columns COLUMN_NAME_NUMBER_OF_ISHURIM, COLUMN_NAME_NUMBER_OF_GORMIM_MEASHRIM
     :param file_df:
-    :return:
+    :return: dataframe after adding the values
     """
     for ind in file_df.index:
         list_of_regulations_as_str = file_df.at[ind, COLUMN_NAME_ISHURIM_LIST]
@@ -97,6 +97,26 @@ def allRegulationCodesInFile(df):
     return result
 
 
+def howManyItemsHaveThisSpecificRegulation3(df1, regulation_code, onlySpecificItemsOptionsDictionaty):
+    # onlySpecificItemsOptionsDictionaty is like {'declarations': False, 'agents': False, 'importers': False}
+    df = addColumnWithCode(df1, regulation_code)
+    # filter the df to retain only lines containing value True in column named "code"
+    # (and possibly only if they have a number > 0 in column COLUMN_NAME_NUMBER_OF_DECLARATIONS)
+    condition = specify_condition(df, onlySpecificItemsOptionsDictionaty)
+    df2 = df[condition]  # <-- this is the filtered dataframe
+    # count how many lines in the dataframe
+    howMany = df2.shape[0]
+    return howMany
+
+
+def howManyItemsHaveEachRegulationCode2(onlySpecificItemsOptionsDictionaty):
+    # {'declarations': False, 'agents': False, 'importers': False}
+    file_df = read_existing_results_file(NAME_OF_SPECIFIC_RESULTS_FILE_WITH_ALL_DATA)
+    regulations = sorted(allRegulationCodesInFile(file_df))
+    results = [howManyItemsHaveThisSpecificRegulation3(file_df, reg, onlySpecificItemsOptionsDictionaty) for reg in regulations]
+    return regulations, results
+
+
 def howManyItemsHaveEachRegulationCode(onlyItemsWithDeclarations = False):
     file_df = read_existing_results_file(NAME_OF_SPECIFIC_RESULTS_FILE_WITH_ALL_DATA)
     all_codes = allRegulationCodesInFile(file_df)
@@ -115,6 +135,48 @@ def howManyItemsHaveEachRegulationCode(onlyItemsWithDeclarations = False):
 #     for reg in regulations:
 #         results.append(howManyItemsHaveThisSpecificRegulation(file_df, reg))
 #     return results
+
+
+def addColumnWithCode(df1, regulation_code):
+    df = df1.copy()
+    df["code"] = ''  # add a column named "code" to the DataFrame
+    for ind in df.index:
+        list_of_regulations_as_str = df.at[ind, COLUMN_NAME_ISHURIM_LIST]
+        list_of_codes = literal_eval(list_of_regulations_as_str)
+        # column COLUMN_NAME_ISHURIM_LIST in the DataFrame contains this list_of_codes,
+        # Now in each cell in column "code" we put True or False according to - if given regulation is in the list of codes
+        df.at[ind, "code"] = regulation_code in list_of_codes
+    return df
+
+
+def howManyItemsHaveThisSpecificRegulation_and_only_agents_or_importers(df1, regulation_code, include_only_items_with_agents = False, include_only_items_with_importers = False):
+    df = addColumnWithCode(df1, regulation_code)
+    # filter the df to retain only lines containing value True in column named "code"
+    # (and possibly only if they have a number > 0 in column COLUMN_NAME_NUMBER_OF_DECLARATIONS)
+    condition = df["code"] == True
+    if include_only_items_with_agents:
+        condition = (df["code"] == True) & (df[COLUMN_NAME_NUMBER_OF_AGENTS] > 0)
+    elif include_only_items_with_importers:
+        condition = (df["code"] == True) & (df[COLUMN_NAME_NUMBER_OF_IMPORTERS] > 0)
+
+    df2 = df[condition]  # <-- this is the filtered dataframe
+
+    # count how many lines in the dataframe
+    howMany = df2.shape[0]
+
+    return howMany
+
+
+def specify_condition(df, onlySpecificItemsOptionsDictionaty):
+    # {'declarations': False, 'agents': False, 'importers': False}
+    condition = df["code"] == True
+    if onlySpecificItemsOptionsDictionaty['declarations']:
+        condition = (df["code"] == True) & (df[COLUMN_NAME_NUMBER_OF_DECLARATIONS] > 0)
+    if onlySpecificItemsOptionsDictionaty['agents']:
+        condition = (df["code"] == True) & (df[COLUMN_NAME_NUMBER_OF_AGENTS] > 0)
+    if onlySpecificItemsOptionsDictionaty['importers']:
+        condition = (df["code"] == True) & (df[COLUMN_NAME_NUMBER_OF_IMPORTERS] > 0)
+    return condition
 
 
 def howManyItemsHaveThisSpecificRegulation(df, regulation_code, include_only_items_with_declarations = False):
@@ -146,6 +208,15 @@ def howManyItemsHaveThisSpecificRegulation(df, regulation_code, include_only_ite
     howMany = df2.shape[0]
 
     return howMany
+
+
+def howManyCustomsItemsNeedEachRegulation2(onlySpecificItemsOptionsDictionaty):
+    # {'declarations': False, 'agents': False, 'importers': False}
+    codes, counts = howManyItemsHaveEachRegulationCode2(onlySpecificItemsOptionsDictionaty)
+    #print("codes", codes, " - counted", counts, "items")
+    counts_dict = dict(zip(codes[:-6], counts[:-6]))    # for convenience I remove last 6 codes because they are Hebrew text
+    print(counts_dict)
+    return counts_dict
 
 
 def howManyCustomsItemsNeedEachRegulation(onlyItemsWithDeclarations = False):
@@ -259,6 +330,62 @@ def do_some_plotting():
     plt.show()
 
 
+def plot_these_data(d1, d2, legend_str_without, legend_str_with, use_percents = True):
+    plt.figure(figsize=[20, 7])
+    pl = plt.bar(d1.keys(), d1.values(), width=0.5, linewidth=2, label=legend_str_without[::-1])
+    index = 0
+    for bar in pl:
+        x_code = list(d1.keys())[index]
+        description_value = bar.get_height()  # if we want description of each bar to be total number of such items
+        if use_percents:
+            percent = 100 * d2[x_code] / d1[x_code]
+            description_value = str(int(percent)) + "%"  # if we want description of each bar to be percent of count without 0 number of declarations from count with any number
+        plt.annotate(description_value, xy=(bar.get_x() - 0.07, bar.get_height() + 10), fontsize=8)
+        index = index + 1
+
+    pl2 = plt.bar(d2.keys(), d2.values(), width=0.7, label=legend_str_with[::-1])
+    plt.title("כמה פריטי מכס צריכים אישור זה"[::-1], fontsize=15)
+    plt.xlabel('קוד אישור'[::-1], fontsize=15)
+    plt.ylabel('מספר פריטים'[::-1], fontsize=15)
+    plt.xticks(fontsize=10, rotation=90)
+    plt.legend()
+    plt.show()
+
+
+def do_plot_customs_items_requiring_each_regulation_for_declarations(calculate_from_excel_file = True, use_percents = True):
+    d1 = {}
+    d2 = {}
+    if calculate_from_excel_file:
+        d1 = howManyCustomsItemsNeedEachRegulation2({'declarations': False, 'agents': False, 'importers': False})
+        d2 = howManyCustomsItemsNeedEachRegulation2({'declarations': True, 'agents': False, 'importers': False})
+    legend_str_without, legend_str_with = ( "כולל ללא הצהרות יבוא", "רק עם הצהרות יבוא" )
+    #legend_str_without, legend_str_with = ( "כולל ללא סוכני מכס", "רק עם סוכני מכס" )
+    #legend_str_without, legend_str_with = ( "כולל ללא יבואנים", "רק עם יבואנים" )
+    plot_these_data(d1, d2, legend_str_without, legend_str_with, use_percents)
+
+
+def do_plot_customs_items_requiring_each_regulation_for_agents(calculate_from_excel_file = True, use_percents = True):
+    d1 = {}
+    d2 = {}
+    if calculate_from_excel_file:
+        d1 = howManyCustomsItemsNeedEachRegulation2({'declarations': False, 'agents': False, 'importers': False})
+        d2 = howManyCustomsItemsNeedEachRegulation2({'declarations': False, 'agents': True, 'importers': False})
+    #legend_str_without, legend_str_with = ( "כולל ללא הצהרות יבוא", "רק עם הצהרות יבוא" )
+    legend_str_without, legend_str_with = ( "כולל ללא סוכני מכס", "רק עם סוכני מכס" )
+    #legend_str_without, legend_str_with = ( "כולל ללא יבואנים", "רק עם יבואנים" )
+    plot_these_data(d1, d2, legend_str_without, legend_str_with, use_percents)
+
+
+def do_plot_customs_items_requiring_each_regulation_for_importers(calculate_from_excel_file = True, use_percents = True):
+    d1 = {}
+    d2 = {}
+    if calculate_from_excel_file:
+        d1 = howManyCustomsItemsNeedEachRegulation2({'declarations': False, 'agents': False, 'importers': False})
+        d2 = howManyCustomsItemsNeedEachRegulation2({'declarations': False, 'agents': False, 'importers': True})
+    #legend_str_without, legend_str_with = ( "כולל ללא הצהרות יבוא", "רק עם הצהרות יבוא" )
+    #legend_str_without, legend_str_with = ( "כולל ללא סוכני מכס", "רק עם סוכני מכס" )
+    legend_str_without, legend_str_with = ( "כולל ללא יבואנים", "רק עם יבואנים" )
+    plot_these_data(d1, d2, legend_str_without, legend_str_with, use_percents)
 
 
 def do_plot_customs_items_requiring_each_regulation(calculate_from_excel_file = False):
@@ -293,8 +420,11 @@ def do_plot_customs_items_requiring_each_regulation(calculate_from_excel_file = 
 
 def main():
     # do_some_plotting()
-    plot_bar_of_number_of_gormim(calculate_dataframe())
-    do_plot_customs_items_requiring_each_regulation()
+    #plot_bar_of_number_of_gormim(calculate_dataframe())
+    #do_plot_customs_items_requiring_each_regulation()
+    do_plot_customs_items_requiring_each_regulation_for_declarations(use_percents=False)
+    do_plot_customs_items_requiring_each_regulation_for_agents(use_percents=False)
+    do_plot_customs_items_requiring_each_regulation_for_importers(use_percents=False)
 
 
 def test():
